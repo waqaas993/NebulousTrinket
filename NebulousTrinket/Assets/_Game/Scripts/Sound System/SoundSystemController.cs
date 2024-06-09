8727fa46d63cceda71010704d71c9fd9d236b13a
Waqas Ahmad <waqas.stu@gmail.com>
2024-06-10 03:55:17 +0500

    Integrated Music & SFX

--- NebulousTrinket/Assets/_Game/Scripts/Sound System/SoundSystemController.cs
+++ NebulousTrinket/Assets/_Game/Scripts/Sound System/SoundSystemController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.Audio;
 
@@ -19,17 +20,26 @@ namespace NebulousTrinket
         public override void Initialize(params object[] parameters)
         {
             Model = new();
+            Play(SoundConsts.BACKGROUND_MUSIC, SoundType.Music);
         }
 
         private void OnEnable()
         {
-
+            FaceCardController.OnFlip += CardFlipped;
+            GamePlayController.OnCardMatched += CardMatched;
+            GamePlayController.OnCardsUnmatched += CardsUnmatched;
         }
 
         private void OnDisable()
         {
-
+            FaceCardController.OnFlip -= CardFlipped;
+            GamePlayController.OnCardMatched -= CardMatched;
+            GamePlayController.OnCardsUnmatched -= CardsUnmatched;
         }
+
+        private void CardFlipped(FaceCardController faceCardController) => Play(SoundConsts.CARD_FLIPPED, SoundType.SFX);
+        private void CardMatched(string obj) => Play(SoundConsts.CARD_MATCHED, SoundType.SFX);
+        private void CardsUnmatched(string arg1, string arg2) => Play(SoundConsts.CARD_UNMATCHED, SoundType.SFX);
 
         private void Play(string sound, SoundType soundType)
         {
@@ -40,7 +50,7 @@ namespace NebulousTrinket
                     MusicPlayer.Play(soundData);
                     break;
                 case SoundType.SFX:
-                    MusicPlayer.Play(soundData);
+                    SFXPlayer.Play(soundData);
                     break;
                 default:
                     break;
